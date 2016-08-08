6f4c9859d9b644c687dc229b1e5d12acdd47bd43
Emeric Henon <emeric.henon@gmail.com>
2016-08-08 16:49:32 +0200

    buen sans style je hais le front

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,11 +7,14 @@ class PagesController < ApplicationController
     10.times do
       @grid << ("a".."z").to_a.sample
     end
+    @start_time = Time.now
   end
 
   def score
     @user_word = params[:user_word]
     @grid = params[:grid].split("")
+    @start_time = params[:start_time].to_datetime
+    @end_time = Time.now
 
     @grid.map!(&:downcase)
     api_url = "http://api.wordreference.com/0.8/80143/json/enfr/#{@user_word}"
@@ -29,15 +32,27 @@ class PagesController < ApplicationController
       # Compter nombre de lettre et score par lettre
       @hash_final[:score] = @user_word.length * 100
       # Compter le temps et score lie au temps
-      # @hash_final[:time] = end_time - start_time
-      # @hash_final[:score] -= @hash_final[:time]
+      @hash_final[:time] = @end_time - @start_time
+      @hash_final[:score] -= @hash_final[:time]
       # Definir le bon message
       @hash_final[:message] = "well done"
     else
       @hash_final[:score] = 0
       @hash_final[:message] = "not in the grid"
     end
-    @hash_final
+    increment_counter
+    session[:score] += @hash_final[:score]
+  end
+
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+    session[:counter] += 1
+    end
+    if session[:score].nil?
+      session[:score] = 0
+    end
   end
 end
 
